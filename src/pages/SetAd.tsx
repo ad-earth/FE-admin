06db22136d309a48dc7f32b54ae17d39518cdbbf
최수인 <choi-suin@choesu-in-ui-MacBookPro.local>
2022-09-19 22:42:30 +0900

    🧹 chore. 프로젝트 셋팅

---
+++ src/pages/SetAd.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const SetAd = () => {
+  return (
+    <div>SetAd</div>
+  )
+}
+
+export default SetAd
---
+++ src/pages/SetProd.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const SetProd = () => {
+  return <div>SetProd</div>;
+};
+
+export default SetProd;
