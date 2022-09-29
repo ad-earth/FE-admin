74fad52a2e923d9b5d1206ed67fa0836562e8932
sol <osunpine@gmail.com>
2022-09-29 23:24:13 +0900

    📝 feat. 배송 관리 테이블 view 개발 (#8)

--- src/components/tables/serviceTable/ServiceTable.tsx
+++ src/components/tables/serviceTable/ServiceTable.tsx
@@ -1,7 +1,57 @@
-import React from "react";
+import "./_serviceTable.style.scss";
+import { testData } from "./test";
 
 const ServiceTable = () => {
-  return <div></div>;
+  // 테이블 제목 리스트
+  const tHeadList = [
+    "No",
+    "주문번호",
+    "상품번호",
+    "상품명",
+    "수량",
+    "아이디",
+    "수령인",
+    "주소",
+    "연락처",
+    "배송메시지",
+    "주문일자",
+    "배송상태",
+  ];
+  return (
+    <table id="orderList">
+      <thead>
+        <th>
+          <input type="checkbox" />
+        </th>
+        {tHeadList.map((x, idx) => {
+          return <th>{x}</th>;
+        })}
+      </thead>
+      <tbody>
+        {testData.map((x, idx) => {
+          return (
+            <tr key={idx}>
+              <td>
+                <input type="checkbox" />
+              </td>
+              <td>{x.id}</td>
+              <td>{x.o_No}</td>
+              <td>{x.p_No}</td>
+              <td>{x.p_Name}</td>
+              <td>{x.p_Cnt}</td>
+              <td>{x.u_Id}</td>
+              <td>{x.d_Name}</td>
+              <td>{x.d_Address1 + x.d_Address2}</td>
+              <td>{x.d_Phone}</td>
+              <td>{x.d_Memo}</td>
+              <td>{x.o_Date}</td>
+              <td>{x.o_Status}</td>
+            </tr>
+          );
+        })}
+      </tbody>
+    </table>
+  );
 };
 
 export default ServiceTable;
