ca8c058f4cc98ab8a9c6acca527f1b922e17c9e6
sol <osunpine@gmail.com>
2022-09-29 23:22:46 +0900

    📝 feat. 배송 관리 페이지 view 개발 (#8)

--- src/components/setParcel/serviceContainer/ServiceContainer.tsx
+++ src/components/setParcel/serviceContainer/ServiceContainer.tsx
@@ -1,7 +1,26 @@
+import Pagination from "@mui/material/Pagination";
+
+import ServiceTable from "../../tables/serviceTable/ServiceTable";
+import SearchBar from "../searchBar/SearchBar";
 import "./_serviceContainer.style.scss";
 
 const ServiceContainer = () => {
-  return <div></div>;
+  return (
+    <div>
+      <h1>배송관리</h1>
+      <div className="tableContainer">
+        <SearchBar />
+        <div className="buttonWrapper">
+          <button>주문확정</button>
+          <button>파일 내려받기</button>
+        </div>
+        <ServiceTable />
+        <div className="pagination">
+          <Pagination count={10} />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default ServiceContainer;
