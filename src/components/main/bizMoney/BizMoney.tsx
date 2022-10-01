efec443770418d1009689f674e210afe9919ef8e
whl5105 <73993670+whl5105@users.noreply.github.com>
2022-10-01 18:06:16 +0900

    📝 feat. 소켓통신 주석처리

--- src/components/main/bizMoney/BizMoney.tsx
+++ src/components/main/bizMoney/BizMoney.tsx
@@ -2,22 +2,22 @@ import React, { memo, useEffect, useState } from "react";
 // import useBizMoney from "./useBizMoney";
 import { io } from "socket.io-client";
 
-const socket = io("http://13.125.243.20:3005", {
-  path: "/socket-admin",
-  transports: ["websocket"],
-  auth: {
-    token: process.env.REACT_APP_TEST_TOKEN,
-  },
-});
+// const socket = io("http://13.125.243.20:3005", {
+//   path: "/socket-admin",
+//   transports: ["websocket"],
+//   auth: {
+//     token: process.env.REACT_APP_TEST_TOKEN,
+//   },
+// });
 
 const BizMoney = () => {
   const [data, setData] = useState(0);
 
-  useEffect(() => {
-    socket.on("biz", (data) => {
-      setData(data.a_Charge);
-    });
-  }, [data]);
+  // useEffect(() => {
+  //   socket.on("biz", (data) => {
+  //     setData(data.a_Charge);
+  //   });
+  // }, [data]);
 
   return <div>{data}</div>;
 };
