a8507dcdc1a1290314ce45bcf1d3d772f121c263
sol <osunpine@gmail.com>
2022-10-02 17:33:22 +0900

    📝 feat. 상품 보고서 테이블 추가 및 페이지 view 작업 (#7)

--- src/components/tables/prodReportTable/ProdReportTable.tsx
+++ src/components/tables/prodReportTable/ProdReportTable.tsx
@@ -1,7 +1,32 @@
+import { testData } from "./test";
 import "./_prodReportTable.style.scss";
 
 const ProdReportTable = () => {
-  return <div></div>;
+  const tHeadList = ["NO", "상품 번호", "상품 명", "판매 수량", "총 판매 금액"];
+  return (
+    <table>
+      <thead>
+        <tr>
+          {tHeadList.map((x, idx) => {
+            return <th key={idx}>{x}</th>;
+          })}
+        </tr>
+      </thead>
+      <tbody>
+        {testData.map((x, idx) => {
+          return (
+            <tr key={x.p_No}>
+              <td>{idx + 1}</td>
+              <td>{x.p_No}</td>
+              <td>{x.p_Name}</td>
+              <td>{x.p_Cnt}</td>
+              <td>{x.p_Price.toLocaleString()}원</td>
+            </tr>
+          );
+        })}
+      </tbody>
+    </table>
+  );
 };
 
 export default ProdReportTable;
