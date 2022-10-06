fa30d519260b7919faf3e2fa141d03636943b384
hyooyh8910 <hyooyh8910@naver.com>
2022-10-07 02:48:12 +0900

    📝 feat.  btn 공통 elements

--- src/elements/buttons/Buttons.tsx
+++ src/elements/buttons/Buttons.tsx
@@ -1,5 +1,7 @@
 import { PropsType } from "./buttons.type";
 import "./_buttons.style.scss";
+import { ReactComponent as Download } from "../../assets/lcon/download.svg";
+import { ReactComponent as Del } from "../../assets/lcon/del.svg";
 
 export const LoginBlueButton = (props: PropsType) => {
   return <button className="login blue">{props.children}</button>;
@@ -13,4 +15,40 @@ export const LoginJiguButton = () => {
       지구샵 바로가기
     </button>
   );
+};
+
+export const SecondBlueBtn = (props: PropsType) => {
+  return <button className="second blue">{props.children}</button>;
+};
+export const SecondWhiteBtn = (props: PropsType) => {
+  return <button className="second white">{props.children}</button>;
+};
+export const ThirdBlueBtn = (props: PropsType) => {
+  return <button className="third blue">{props.children}</button>;
+};
+export const ThirdWhiteBtn = (props: PropsType) => {
+  return <button className="third white">{props.children}</button>;
+};
+export const ForthBtn = (props: PropsType) => {
+  return <button className="forth">{props.children}</button>;
+};
+export const FifthDownBtn = (props: PropsType) => {
+  return (
+    <button className="fifth download">
+      <Download style={{ marginRight: "7px" }} />
+      파일 내려받기
+    </button>
+  );
+};
+export const FifthDelBtn = (props: PropsType) => {
+  return (
+    <button className="fifth delete">
+      <Del style={{ marginRight: "5px" }} />
+      상품삭제
+    </button>
+  );
+};
+
+export const SixthBtn = (props: PropsType) => {
+  return <button className="sixth">{props.children}</button>;
 };
