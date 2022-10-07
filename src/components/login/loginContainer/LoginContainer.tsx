67a4bf9f3debfbaf13946ac3fc06a6eaec6ca19c
sol <osunpine@gmail.com>
2022-10-07 13:37:37 +0900

    📝 feat. style module

--- src/components/login/loginContainer/LoginContainer.tsx
+++ src/components/login/loginContainer/LoginContainer.tsx
@@ -1,6 +1,6 @@
 import { useNavigate } from "react-router-dom";
 
-import "./_loginContainer.style.scss";
+import styles from "./loginContainer.module.scss";
 import logo from "../../../assets/logo/logo.svg";
 import LoginInput from "../../../elements/inputs/Inputs";
 import {
@@ -11,20 +11,20 @@ import {
 const LoginContainer = () => {
   const navigate = useNavigate();
   return (
-    <div className="container">
-      <div className="loginWrapper">
-        <img src={logo} className="logo" />
-        <p className="loginTitle">사업자 로그인</p>
-        <div className="inputWrapper">
+    <div className={styles.container}>
+      <div className={styles.loginWrapper}>
+        <img src={logo} className={styles.logo} />
+        <p className={styles.loginTitle}>사업자 로그인</p>
+        <div className={styles.inputWrapper}>
           <LoginInput placeholder="아이디" type="text" />
           <LoginInput placeholder="비밀번호" type="password" />
         </div>
-        <p className="errorMsg">아이디 비밀번호를 다시 확인해주세요.</p>
-        <div className="buttonWrapper">
+        <p className={styles.errorMsg}>아이디 비밀번호를 다시 확인해주세요.</p>
+        <div className={styles.buttonWrapper}>
           <LoginBlueButton>로그인</LoginBlueButton>
           <LoginJiguButton />
         </div>
-        <div className="buttonWrapper row">
+        <div className={`${styles.buttonWrapper} ${styles.row}`}>
           <button onClick={() => navigate("/signup")}>회원가입하기</button>|
           <button onClick={() => navigate("/find_account")}>
             아이디 비밀번호 찾기
