5196e6debe388926b58c48618a55f1cd7d7e8dfb
damiiya <ddam2lee@gmail.com>
2022-10-09 06:09:00 +0900

    📝 feat. PostProd.tsx 드롭다운 테스트 코드 참고용 작성

--- src/pages/PostProd.tsx
+++ src/pages/PostProd.tsx
@@ -1,7 +1,34 @@
-import React from "react";
+import { useState } from "react";
+import { MediumDropdown } from "../elements/dropdown/DropDown";
+import styles from "./styles/postProd.module.scss";
+
+// 드롭다운 테스트 데이터
+const names = [
+  "dkadddd",
+  "Van Henry",
+  "April Tucker",
+  "Ralph Hubbard",
+  "Omar Alexander",
+  "Carlos Abbott",
+  "Miriam Wagner",
+  "Bradley Wilkerson",
+  "Virginia Andrews",
+  "Kelly Snyder",
+];
 
 const PostProd = () => {
-  return <div>PostProd</div>;
+  // 드롭다운 defaultValue는 useState에 담기는 배열 첫번째 값으로 직접 넣어줌
+  const [selected, setSelected] = useState(names[0]);
+
+  return (
+    <div className={styles.container}>
+      <MediumDropdown
+        itemList={names}
+        selected={selected}
+        setSelected={setSelected}
+      />
+    </div>
+  );
 };
 
 export default PostProd;
