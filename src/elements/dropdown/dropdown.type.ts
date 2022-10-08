2f201d95b7bb9f447befa21c66cd6162812e2224
damiiya <ddam2lee@gmail.com>
2022-10-09 06:03:45 +0900

    📝 feat. dropdown.type.ts 파일 작성 완료

--- src/elements/dropdown/dropdown.type.ts
+++ src/elements/dropdown/dropdown.type.ts
@@ -1,1 +1,8 @@
-export interface PropsType {}
+import React from "react";
+
+export interface PropsType {
+  itemList: string[];
+  selected?: string;
+  setSelected?: (val: string) => void;
+  onChange?: () => void;
+}
