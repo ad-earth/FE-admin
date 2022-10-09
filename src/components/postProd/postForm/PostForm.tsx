a10577a658e6ae9d256169bc2c853b4edc260dd5
damiiya <ddam2lee@gmail.com>
2022-10-09 09:17:12 +0900

    📝 feat. 코드 수정

--- src/components/postProd/postForm/PostForm.tsx
+++ src/components/postProd/postForm/PostForm.tsx
@@ -1,7 +1,19 @@
-import React from "react";
+import { useState } from "react";
+import { MediumDropdown } from "../../../elements/dropdown/DropDown";
+
+const category = ["욕실", "주방", "음료용품", "생활", "식품", "화장품", "문구"];
 
 const PostForm = () => {
-  return <div>postForm</div>;
+  const [selected, setSelected] = useState(category[0]);
+  return (
+    <div>
+      <MediumDropdown
+        itemList={category}
+        selected={selected}
+        setSelected={setSelected}
+      />
+    </div>
+  );
 };
 
 export default PostForm;
--- src/pages/PostProd.tsx
+++ src/pages/PostProd.tsx
@@ -1,32 +1,13 @@
-import { useState } from "react";
-import { MediumDropdown } from "../elements/dropdown/DropDown";
 import styles from "./styles/postProd.module.scss";
-
-// 드롭다운 테스트 데이터
-const names = [
-  "dkadddd",
-  "Van Henry",
-  "April Tucker",
-  "Ralph Hubbard",
-  "Omar Alexander",
-  "Carlos Abbott",
-  "Miriam Wagner",
-  "Bradley Wilkerson",
-  "Virginia Andrews",
-  "Kelly Snyder",
-];
+import PostForm from "../components/postProd/postForm/PostForm";
 
 const PostProd = () => {
-  // 드롭다운 defaultValue는 useState에 담기는 배열 첫번째 값으로 직접 넣어줌
-  const [selected, setSelected] = useState(names[0]);
-
   return (
     <div className={styles.container}>
-      <MediumDropdown
-        itemList={names}
-        selected={selected}
-        setSelected={setSelected}
-      />
+      <p className={styles.title}>상품등록</p>
+      <div className={styles.contentWrapper}>
+        <PostForm />
+      </div>
     </div>
   );
 };
