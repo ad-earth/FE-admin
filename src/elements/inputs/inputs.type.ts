55e42963d8ef5d4a7c260f3a1bcec6c6b69a6325
whl5105 <73993670+whl5105@users.noreply.github.com>
2022-10-11 21:41:06 +0900

    🐛 fix. input type 추가

--- src/elements/inputs/inputs.type.ts
+++ src/elements/inputs/inputs.type.ts
@@ -2,5 +2,7 @@ export interface PropsType {
   placeholder: string;
   type?: string;
   value?: string;
+  name?: string;
   setInput?: (val: string) => void;
+  onChange?: (e: React.ChangeEvent<HTMLInputElement>) => void;
 }
