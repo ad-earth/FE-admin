bf99c6e334d70cb354e5be048998bf55ea427505
whl5105 <73993670+whl5105@users.noreply.github.com>
2022-10-12 03:52:53 +0900

    📝 feat. header 페이지 이동 구현

--- src/components/layouts/header/Header.tsx
+++ src/components/layouts/header/Header.tsx
@@ -1,14 +1,31 @@
 import React from "react";
 import styles from "./header.module.scss";
 import logo from "../../../assets/logo/header.svg";
+import { SmallWhiteBtn } from "../../../elements/buttons/Buttons";
+import { useNavigate } from "react-router-dom";
 
 const Header = () => {
+  const navigate = useNavigate();
+  //상품등록 페이지 이동
+  const btnClick = () => navigate({ pathname: "/PostProd" });
+  //메인 페이지 이동
+  const logoClick = () => navigate({ pathname: "/home" });
+  //로그아웃 클릭
+  const logoutClcik = () => {
+    if (window.confirm("로그아웃 하시겠습니까?")) {
+      localStorage.clear();
+      navigate("/", { replace: true });
+    } else {
+      return;
+    }
+  };
+
   return (
     <header>
-      <img src={logo} alt="logo" />
+      <img src={logo} alt="logo" onClick={logoClick} />
       <div className={styles.headerRight}>
-        <button>상품등록</button>
-        <span>로그아웃</span>
+        <SmallWhiteBtn onClick={btnClick}>상품등록</SmallWhiteBtn>
+        <span onClick={logoutClcik}>로그아웃</span>
       </div>
     </header>
   );
