5d33aa8dc4c79ee369d38663ddf8996f364de932
whl5105 <73993670+whl5105@users.noreply.github.com>
2022-10-14 20:32:21 +0900

    📝 feat.adBanner click event 코드추가

--- src/components/banner/adBanner/AdBanner.tsx
+++ src/components/banner/adBanner/AdBanner.tsx
@@ -1,5 +1,6 @@
 import styles from "./adBanner.module.scss";
 import character from "../../../assets/bannerCharacter.svg";
+import { useNavigate } from "react-router-dom";
 
 //text
 const title = "광고지구로 비즈니스 성장을 이루세요";
@@ -9,8 +10,9 @@ const subText2 =
   "광고 입찰가를 높일수록 최상단에 광고가 노출됩니다. 지금 키워드 광고를등록해 보세요!";
 
 const AdBanner = () => {
+  const navigate = useNavigate();
   return (
-    <section id={styles.adBanner}>
+    <section id={styles.adBanner} onClick={() => navigate("/setAd")}>
       <h2 className={styles.title}>{title}</h2>
       <p className={styles.subText}>
         {subText1}
