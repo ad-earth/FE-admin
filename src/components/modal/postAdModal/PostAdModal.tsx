42feb27a7348c0289cf848e5d75518956d2de7a2
whl5105 <73993670+whl5105@users.noreply.github.com>
2022-10-17 14:45:08 +0900

    📝 feat. 코드수정

--- src/components/modal/postAdModal/PostAdModal.tsx
+++ src/components/modal/postAdModal/PostAdModal.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
-const postAdModal = () => {
+const PostAdModal = () => {
   return <div>postAdModal</div>;
 };
 
-export default postAdModal;
+export default PostAdModal;
