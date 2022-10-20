5f4c5c0a76d5c0339925bbf8d593f11a26ddde66
sol <osunpine@gmail.com>
2022-10-21 00:31:27 +0900

    📝 feat. 쿼리 옵션 추가 (#20)

--- src/components/account/findIdForm/useFindIdQuery.ts
+++ src/components/account/findIdForm/useFindIdQuery.ts
@@ -2,5 +2,7 @@ import { useQuery } from "react-query";
 import { getId } from "../../../shared/apis/api";
 
 export const useFindIdQuery = (brand: string, businessNumber: string) => {
-  return useQuery("findId", () => getId(brand, businessNumber));
+  return useQuery("findId", () => getId(brand, businessNumber), {
+    enabled: false,
+  });
 };
