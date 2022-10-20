9c44137f0a12018368dc543af6a2a5d143b621a2
sol <osunpine@gmail.com>
2022-10-20 22:56:52 +0900

    📝 feat. 에러 처리 & 엔터 입력키 활성화 (#20)

---
+++ src/components/account/findIdForm/useFindIdQuery.ts
@@ -0,0 +1,6 @@
+import { useQuery } from "react-query";
+import { getId } from "../../../shared/apis/api";
+
+export const useFindIdQuery = (brand: string, businessNumber: string) => {
+  return useQuery("findId", () => getId(brand, businessNumber));
+};
