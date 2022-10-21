d112e66262c3592ba5c7020fbe41efe4d4cd24b8
sol <osunpine@gmail.com>
2022-10-22 06:11:46 +0900

    📝 feat. 광고페이지 filter data 상품명에서 상품번호로 변경

--- src/components/adReport/adFilter/useProductQuery.ts
+++ src/components/adReport/adFilter/useProductQuery.ts
@@ -1,6 +1,11 @@
 import { useQuery } from "react-query";
-import { getSalesReport } from "../../../shared/apis/api";
+import { getAd } from "../../../shared/apis/api";
 
-export const useProductQuery = (date: string) => {
-  return useQuery("sales", () => getSalesReport(null, date));
+export const useProductQuery = () => {
+  const { data } = useQuery("productNumbers", () => getAd());
+  let numberList: string[] = [];
+  data?.data.productList.map(({ p_No }: { p_No: number; p_Name: string }) => {
+    numberList.push(String(p_No));
+  });
+  return numberList;
 };
