5c6492e497c2b2101c1f1a8bfb9a94ab6ea495f3
sol <osunpine@gmail.com>
2022-10-21 23:16:24 +0900

    📂 docs. 파일 삭제

--- src/components/prodReport/prodContainer/useDate.ts
+++
@@ -1,8 +0,0 @@
-export const useDate = () => {
-  const today = new Date();
-  const year = today.getFullYear();
-  const month = ("0" + (today.getMonth() + 1)).slice(-2);
-  const minMonth = "0" + (today.getMonth() - 3);
-  const day = ("0" + today.getDate()).slice(-2);
-  return `[${year}-${minMonth}-${day},${year}-${month}-${day}]`;
-};
