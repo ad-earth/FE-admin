e431c2d48fad366b928e79d6a96ffcfd69e192ee
whl5105 <73993670+whl5105@users.noreply.github.com>
2022-10-26 17:06:28 +0900

    🐛 fix.  모달open 스크롤 버그 수정

--- src/components/modal/useModal.ts
+++ src/components/modal/useModal.ts
@@ -4,10 +4,15 @@ import { ModalType, modalState } from "../../store/modal";
 export default function useModal() {
   const [modal, setModal] = useRecoilState(modalState);
 
-  const showModal = ({ modalType, modalProps }: ModalType) =>
+  const showModal = ({ modalType, modalProps }: ModalType) => {
     setModal({ modalType, modalProps });
+    document.body.style.overflow = "hidden"; //모달 스크롤 방지
+  };
 
-  const hideModal = () => setModal(null);
+  const hideModal = () => {
+    setModal(null);
+    document.body.style.overflow = "unset"; //모달 스크롤 방지
+  };
 
   return {
     modal,
