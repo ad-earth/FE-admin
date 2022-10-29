639667f71980ce556464e5270cfbe1fa166e81f7
whl5105 <73993670+whl5105@users.noreply.github.com>
2022-10-29 12:05:02 +0900

    📝 feat. GlobalModal  코드 정리

--- src/components/modal/GlobalModal.tsx
+++ src/components/modal/GlobalModal.tsx
@@ -1,34 +1,29 @@
-import React from "react";
 import { useRecoilState } from "recoil";
 import { modalState } from "../../store/modal";
 import WithdrawalModal from "./withdrawalModal/WithdrawalModal";
 import LogoutModal from "./logoutModal/LogoutModal";
 import PostAdModal from "./postAdModal/PostAdModal";
 
-export const MODAL_TYPES = {
+export const modalTypes = {
   WithdrawalModal: "WithdrawalModal",
   LogoutModal: "LogoutModal",
   PostAdModal: "PostAdModal",
 } as const;
 
-const modalComponents: any = {
-  [MODAL_TYPES.WithdrawalModal]: WithdrawalModal,
-  [MODAL_TYPES.LogoutModal]: LogoutModal,
-  [MODAL_TYPES.PostAdModal]: PostAdModal,
+const modalComponents = {
+  [modalTypes.WithdrawalModal]: WithdrawalModal,
+  [modalTypes.LogoutModal]: LogoutModal,
+  [modalTypes.PostAdModal]: PostAdModal,
 };
 
 const GlobalModal = () => {
   const { modalType, modalProps } = useRecoilState(modalState)[0] || {};
 
   const renderComponent = () => {
-    if (!modalType) {
-      return null;
-    }
+    if (!modalType) return null;
     const ModalComponent = modalComponents[modalType];
-
     return <ModalComponent {...modalProps} />;
   };
-
   return <>{renderComponent()}</>;
 };
 
