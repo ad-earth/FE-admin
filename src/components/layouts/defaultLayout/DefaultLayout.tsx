fa0e62f0b4c4d7e785af2591feaa5e3d0dda2df6
whl5105 <73993670+whl5105@users.noreply.github.com>
2022-10-29 11:13:03 +0900

    📝 feat. 레이아웃 코드 정리

--- src/components/layouts/defaultLayout/DefaultLayout.tsx
+++ src/components/layouts/defaultLayout/DefaultLayout.tsx
@@ -2,18 +2,18 @@ import { useEffect } from "react";
 import styles from "./defaultLayout.module.scss";
 import { Outlet } from "react-router-dom";
 import { useNavigate } from "react-router-dom";
-
-//components
 import Header from "../header/Header";
 import Nav from "../nav/Nav";
 
 const DefaultLayout = () => {
   const navigate = useNavigate();
+  const token = localStorage.getItem("token");
 
   // 로그인 유저 확인
   useEffect(() => {
-    !localStorage.getItem("token") && navigate("/");
-  }, []);
+    if (token) return;
+    navigate("/");
+  }, [token]);
 
   return (
     <div id={styles.DefaultLayout}>
