2a03d4ee52e8def3ea9d2fce647d2ba74501ae88
whl5105 <73993670+whl5105@users.noreply.github.com>
2022-11-04 14:32:16 +0900

    🐛 fix. 회원탈퇴 에러 toast 코드 삭제
    - 전역 에러 처리 코드 추가로 인해  불필요한 코드 삭제

--- src/components/modal/withdrawalModal/WithdrawalModal.tsx
+++ src/components/modal/withdrawalModal/WithdrawalModal.tsx
@@ -1,32 +1,20 @@
-import { useState, useEffect } from "react";
 import styles from "./withdrawalModal.module.scss";
 import { useNavigate } from "react-router-dom";
 import { ModalCancelBtn, ModalDelBtn } from "../../../elements/buttons/Buttons";
 import useModal from "../useModal";
 import { useWithdrawalQuery } from "./useWithdrawalQuery";
-import Snackbar from "@mui/material/Snackbar";
-import MuiAlert from "@mui/material/Alert";
 import { WithdrawalType } from "./withdrawalModal.type";
 
 const WithdrawalModal = ({ title }: WithdrawalType) => {
   const navigate = useNavigate();
-  const [errMsg, setErrMsg] = useState<string>();
-  //toast alert on/off
-  const [open, setOpen] = useState(false);
   const { hideModal } = useModal();
-  const { mutate, isError, isSuccess, error } = useWithdrawalQuery();
+  const { mutate, isSuccess } = useWithdrawalQuery();
   //회원탈퇴 성공시
   if (isSuccess) {
     localStorage.clear();
     navigate("/", { replace: true });
     hideModal();
   }
-  //회원탈퇴 에러처리
-  useEffect(() => {
-    if (!isError) return;
-    setErrMsg(error.response.data.errorMessage);
-    setOpen(true);
-  }, [isError]);
 
   return (
     <div className={styles.withdrawal}>
@@ -37,14 +25,6 @@ const WithdrawalModal = ({ title }: WithdrawalType) => {
           <ModalDelBtn onClick={() => mutate()}>탈퇴하기</ModalDelBtn>
         </div>
       </div>
-      <Snackbar
-        anchorOrigin={{ vertical: "bottom", horizontal: "center" }}
-        open={open}
-        autoHideDuration={2000}
-        onClose={() => setOpen(false)}
-      >
-        <MuiAlert severity="error">{errMsg}</MuiAlert>
-      </Snackbar>
     </div>
   );
 };
