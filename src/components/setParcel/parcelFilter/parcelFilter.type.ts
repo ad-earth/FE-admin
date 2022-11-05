7f72a73e02640d41440f48337f3772047529ba26
Haesol <osunpine@gmail.com>
2022-11-06 01:26:51 +0900

    🧹 chore. 파일 삭제

--- src/components/setParcel/parcelFilter/parcelFilter.type.ts
+++
@@ -1,5 +0,0 @@
-export interface PropsType {
-  setDate: (date: string) => void;
-  setProduct: (product: string) => void;
-  setStatus: (status: string) => void;
-}
--- src/elements/datePicker/useInitialDate.ts
+++
@@ -1,8 +0,0 @@
-export const useInitialDate = () => {
-  const today = new Date();
-  const year = today.getFullYear();
-  const month1 = "0" + today.getMonth();
-  const month2 = ("0" + (today.getMonth() + 1)).slice(-2);
-  const day = ("0" + today.getDate()).slice(-2);
-  return [`${year}-${month1}-${day}`, `${year}-${month2}-${day}`];
-};
