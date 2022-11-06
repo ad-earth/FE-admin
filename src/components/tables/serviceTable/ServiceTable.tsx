589006ac113cbc145537e891557c3f2a50fed71e
Haesol <osunpine@gmail.com>
2022-11-06 14:59:59 +0900

    📝 feat. table style 변경

--- src/components/tables/serviceTable/ServiceTable.tsx
+++ src/components/tables/serviceTable/ServiceTable.tsx
@@ -5,10 +5,8 @@ import Pagination from "@mui/material/Pagination";
 import styles from "./serviceTable.module.scss";
 import { ReactComponent as Download } from "../../../assets/lcon/download.svg";
 import { SmallGrayBtn } from "../../../elements/buttons/Buttons";
-import { useHeaderList } from "./useHeaderList";
 import { useExcelData } from "./useExcelData";
 import { useExcelQuery } from "./useExcelQuery";
-import { useExcelHeaderList } from "./useExcelHeaderList";
 import { useGetOrderListQuery } from "./useGetOrderListQuery";
 import { useRecoilState } from "recoil";
 import {
@@ -18,6 +16,7 @@ import {
   selectedStatusState,
 } from "../../../store/filter";
 import { OrderListType } from "./serviceTable.type";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
 
 const ServiceTable = () => {
   const [confirmList, setConfirmList] = useState([]);
@@ -95,15 +94,13 @@ const ServiceTable = () => {
     }
   };
 
-  const excelHeaders = useExcelHeaderList();
-  const tableHeaders = useHeaderList();
   return (
     <div>
       <div className={styles.buttonWrapper}>
         <SmallGrayBtn>주문확정</SmallGrayBtn>
         <button className={styles.download}>
           <CSVLink
-            headers={excelHeaders}
+            headers={excelHeaderList}
             data={excelData}
             filename="order_list.csv"
             target="_blank"
@@ -113,80 +110,137 @@ const ServiceTable = () => {
           </CSVLink>
         </button>
       </div>
-      <table id="orderList" className={styles.tableContainer}>
-        <thead className={styles.tHead}>
-          <tr className={styles.tr}>
-            <th className={styles.th}>
-              <input
-                type="checkbox"
-                onChange={(e) => handleAllCheck(e.target.checked)}
-                checked={
-                  checkedItems.length === orderListQuery.data?.data.length
-                    ? true
-                    : false
-                }
-              />
-            </th>
-            {tableHeaders.map((header, idx) => {
-              return <th key={idx}>{header}</th>;
-            })}
-          </tr>
-        </thead>
-        <tbody className={styles.tBody}>
-          {orderList?.map((x: OrderListType) => (
-            <tr key={x.id}>
-              <td>
+      <div className={styles.tableContainer}>
+        <table id="orderList">
+          <thead>
+            <tr>
+              <th>
                 <input
                   type="checkbox"
-                  onChange={(e) =>
-                    handleSingleCheck(e.target.checked, x.id, x.o_No, x.p_No)
+                  onChange={(e) => handleAllCheck(e.target.checked)}
+                  checked={
+                    checkedItems.length === orderListQuery.data?.data.length
+                      ? true
+                      : false
                   }
-                  checked={checkedItems.includes(x.id) ? true : false}
                 />
-              </td>
-              <td>{x.id}</td>
-              <td>{x.o_No}</td>
-              <td>{x.p_No}</td>
-              <td>
-                <Tooltip
-                  title={x.p_Option.map((option) => {
-                    return `${option[0] ? option[0] : ""}${
-                      option[1] ? option[1] : ""
-                    }X${option[3]}  `;
-                  })}
-                  arrow
-                >
-                  <span>{x.p_Name}</span>
-                </Tooltip>
-              </td>
-              <td>{x.p_Cnt}</td>
-              <td>{x.u_Id}</td>
-              <td>{x.d_Name}</td>
-              <td>
-                <Tooltip
-                  title={`[${x.d_Address1}]  ${x.d_Address2} ${x.d_Address3}`}
-                  arrow
-                >
-                  <span>{x.d_Address2}</span>
-                </Tooltip>
-              </td>
-              <td>{x.d_Phone}</td>
-              <td>{x.d_Memo}</td>
-              <td>{x.o_Date}</td>
-              <td>{x.o_Status}</td>
+              </th>
+              {headerList.map((header, idx) => {
+                return <th key={idx}>{header}</th>;
+              })}
             </tr>
-          ))}
-        </tbody>
-      </table>
-      <div className={styles.pagination}>
+          </thead>
+          {orderList?.length ? (
+            <tbody>
+              {orderList?.map((x: OrderListType) => (
+                <tr key={x.id}>
+                  <td>
+                    <input
+                      type="checkbox"
+                      onChange={(e) =>
+                        handleSingleCheck(
+                          e.target.checked,
+                          x.id,
+                          x.o_No,
+                          x.p_No
+                        )
+                      }
+                      checked={checkedItems.includes(x.id) ? true : false}
+                    />
+                  </td>
+                  <td>{x.id}</td>
+                  <td>{x.o_No}</td>
+                  <td>{x.p_No}</td>
+                  <td>
+                    <Tooltip
+                      title={x.p_Option.map((option) => {
+                        return `${option[0] ? option[0] : ""}${
+                          option[1] ? option[1] : ""
+                        }X${option[3]}  `;
+                      })}
+                      arrow
+                    >
+                      <span>{x.p_Name}</span>
+                    </Tooltip>
+                  </td>
+                  <td>{x.p_Cnt}</td>
+                  <td>{x.u_Id}</td>
+                  <td>{x.d_Name}</td>
+                  <td>
+                    <Tooltip
+                      title={`[${x.d_Address1}]  ${x.d_Address2} ${x.d_Address3}`}
+                      arrow
+                    >
+                      <span>{x.d_Address2}</span>
+                    </Tooltip>
+                  </td>
+                  <td>{x.d_Phone}</td>
+                  <td>{x.d_Memo}</td>
+                  <td>{x.o_Date}</td>
+                  <td>{x.o_Status}</td>
+                </tr>
+              ))}
+            </tbody>
+          ) : (
+            <tbody>
+              <tr>
+                <td className={styles.noData}>해당하는 상품이 없습니다.</td>
+              </tr>
+            </tbody>
+          )}
+        </table>
+      </div>
+
+      <ThemeProvider theme={theme}>
         <Pagination
+          className={styles.pagination}
           count={Math.ceil(dataQty / 10)}
           page={page}
           onChange={handlePage}
+          variant="outlined"
+          shape="rounded"
+          color="primary"
+          boundaryCount={3}
         />
-      </div>
+      </ThemeProvider>
     </div>
   );
 };
 
 export default ServiceTable;
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#4e60ff",
+    },
+  },
+});
+export const headerList = [
+  "No",
+  "주문번호",
+  "상품번호",
+  "상품명",
+  "수량",
+  "아이디",
+  "수령인",
+  "주소",
+  "연락처",
+  "배송메시지",
+  "주문일자",
+  "배송상태",
+];
+
+export const excelHeaderList = [
+  { label: "주문번호", key: "orderNo" },
+  { label: "상품번호", key: "prodNo" },
+  { label: "상품명", key: "prodName" },
+  { label: "수량", key: "prodQty" },
+  { label: "아이디", key: "userId" },
+  { label: "수령인", key: "userName" },
+  { label: "주소", key: "address" },
+  { label: "연락처", key: "phone" },
+  { label: "배송메시지", key: "comment" },
+  { label: "주문일자", key: "orderDate" },
+  { label: "배송상태", key: "status" },
+];
