960aef25402b5a63d9c3e22dd563c07ddcf60dc5
Haesol <osunpine@gmail.com>
2022-11-06 15:25:04 +0900

    📝 feat. table style 변경

--- src/components/tables/prodReportTable/ProdReportTable.tsx
+++ src/components/tables/prodReportTable/ProdReportTable.tsx
@@ -1,15 +1,8 @@
 import styles from "./prodReportTable.module.scss";
-import { useGetSalesQuery } from "../../prodReport/prodContainer/useGetSalesQuery";
-import { useRecoilState } from "recoil";
-import {
-  selectedCategoryState,
-  selectedEndDateState,
-  selectedStartDateState,
-} from "../../../store/filter";
 import { PropsType } from "./prodReportTable.type";
 
 const ProdReportTable = (props: PropsType) => {
-  const tableHeaders = [
+  const headerList = [
     "NO",
     "상품 번호",
     "상품 명",
@@ -18,30 +11,36 @@ const ProdReportTable = (props: PropsType) => {
   ];
 
   return (
-    <table className={styles.tableContainer}>
-      <thead className={styles.tHead}>
-        <tr className={styles.tr}>
-          {tableHeaders.map((header, idx) => {
-            return (
-              <th className={styles.th} key={idx}>
-                {header}
-              </th>
-            );
-          })}
-        </tr>
-      </thead>
-      <tbody className={styles.tBody}>
-        {props.productData?.map((x, idx) => (
-          <tr className={styles.tr} key={x.p_No}>
-            <td className={styles.td}>{idx + 1}</td>
-            <td className={styles.td}>{x.p_No}</td>
-            <td className={styles.td}>{x.p_Name}</td>
-            <td className={styles.td}>{x.p_Cnt}</td>
-            <td className={styles.td}>{x.p_Price.toLocaleString()}원</td>
+    <div className={styles.salesTable}>
+      <table>
+        <thead>
+          <tr>
+            {headerList.map((header, idx) => {
+              return <th key={idx}>{header}</th>;
+            })}
           </tr>
-        ))}
-      </tbody>
-    </table>
+        </thead>
+        {props.productData?.length ? (
+          <tbody>
+            {props.productData?.map((x, idx) => (
+              <tr key={x.p_No}>
+                <td>{idx + 1}</td>
+                <td>{x.p_No}</td>
+                <td>{x.p_Name}</td>
+                <td>{x.p_Cnt}</td>
+                <td>{x.p_Price.toLocaleString()}원</td>
+              </tr>
+            ))}
+          </tbody>
+        ) : (
+          <tbody>
+            <tr>
+              <td className={styles.noData}>해당하는 상품이 없습니다.</td>
+            </tr>
+          </tbody>
+        )}
+      </table>
+    </div>
   );
 };
 
