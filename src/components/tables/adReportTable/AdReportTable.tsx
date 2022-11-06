1c7bb91e1afbe2f9440823af59866fb3ba410000
Haesol <osunpine@gmail.com>
2022-11-06 16:32:10 +0900

    📝 feat. table style 변경

--- src/components/tables/adReportTable/AdReportTable.tsx
+++ src/components/tables/adReportTable/AdReportTable.tsx
@@ -2,7 +2,7 @@ import styles from "./adReportTable.module.scss";
 import { PropsType } from "./adReportTable.type";
 
 const AdReportTable = (props: PropsType) => {
-  const tableHeaders = [
+  const headerList = [
     "NO",
     "키워드",
     "클릭 수",
@@ -14,56 +14,52 @@ const AdReportTable = (props: PropsType) => {
     "수익율",
   ];
   return (
-    <table className={styles.tableContainer}>
-      <thead className={styles.tHead}>
-        <tr>
-          {tableHeaders.map((x, idx) => {
-            return (
-              <th key={idx} className={styles.th}>
-                {x}
-              </th>
-            );
-          })}
-        </tr>
-      </thead>
-      <tbody className={styles.tBody}>
-        {props.dataList && props.dataList.length ? (
-          props.dataList.map((x, idx) => {
-            return (
-              <tr key={x.keyword} className={styles.tr}>
-                <td className={styles.td}>{idx + 1}</td>
-                <td className={styles.td}>{x.keyword}</td>
-                <td className={styles.td}>{x.k_Click}</td>
-                <td className={styles.td}>
-                  {Math.round(x.k_Cost / x.k_Click).toLocaleString()}원
-                </td>
-                <td className={styles.td}>{x.k_Cost.toLocaleString()}원</td>
-                <td className={styles.td}>{x.k_Trans}</td>
-                <td className={styles.td}>
-                  {Math.round((x.k_Trans / x.k_Click) * 100)}%
-                </td>
-                <td className={styles.td}>{x.p_Price.toLocaleString()}원</td>
-                <td className={styles.td}>
-                  {Math.round((x.p_Price / x.k_Cost) * 100)}%
-                </td>
-              </tr>
-            );
-          })
-        ) : (
-          <tr className={styles.tr}>
-            <td className={`${styles.td} ${styles.hidden}`}>0</td>
-            <td className={`${styles.td} ${styles.hidden}`}>0000000</td>
-            <td className={`${styles.td} ${styles.hidden}`}>000</td>
-            <td className={`${styles.td} ${styles.hidden}`}>0000000</td>
-            <td className={styles.td}>등록된 상품이 없습니다.</td>
-            <td className={`${styles.td} ${styles.hidden}`}>000</td>
-            <td className={`${styles.td} ${styles.hidden}`}>000</td>
-            <td className={`${styles.td} ${styles.hidden}`}>00000000원</td>
-            <td className={`${styles.td} ${styles.hidden}`}>0000000</td>
+    <div className={styles.adTable}>
+      <table>
+        <thead>
+          <tr>
+            {headerList.map((x, idx) => {
+              return (
+                <th key={idx} className={styles.th}>
+                  {x}
+                </th>
+              );
+            })}
           </tr>
+        </thead>
+        {props.dataList?.length ? (
+          <tbody>
+            {props.dataList?.map((x, idx) => {
+              return (
+                <tr key={x.keyword} className={styles.tr}>
+                  <td className={styles.td}>{idx + 1}</td>
+                  <td className={styles.td}>{x.keyword}</td>
+                  <td className={styles.td}>{x.k_Click}</td>
+                  <td className={styles.td}>
+                    {Math.round(x.k_Cost / x.k_Click).toLocaleString()}원
+                  </td>
+                  <td className={styles.td}>{x.k_Cost.toLocaleString()}원</td>
+                  <td className={styles.td}>{x.k_Trans}</td>
+                  <td className={styles.td}>
+                    {Math.round((x.k_Trans / x.k_Click) * 100)}%
+                  </td>
+                  <td className={styles.td}>{x.p_Price.toLocaleString()}원</td>
+                  <td className={styles.td}>
+                    {Math.round((x.p_Price / x.k_Cost) * 100)}%
+                  </td>
+                </tr>
+              );
+            })}
+          </tbody>
+        ) : (
+          <tbody>
+            <tr>
+              <td className={styles.noData}>해당하는 상품이 없습니다.</td>
+            </tr>
+          </tbody>
         )}
-      </tbody>
-    </table>
+      </table>
+    </div>
   );
 };
 
